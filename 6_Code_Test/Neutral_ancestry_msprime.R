5a78e7ccebfb7474bf7a6f32e1c82b0b9b1969db
manas-ga <manas.geetaarun@ed.ac.uk>
2024-02-05 15:25:15 +0000

    Update Neutral_ancestry_msprime.R
    Added another argument to the function (python_path)

--- 6_Code_Test/Neutral_ancestry_msprime.R
+++ 6_Code_Test/Neutral_ancestry_msprime.R
@@ -3,9 +3,12 @@
 ###############################################################################################
 
 # source("/mnt/c/Users/msamant/Documents/GitHub/Va_simulations/6_Code_test/Neutral_ancestry_msprime.R")
+# python_path = "/home/manas_ga/miniconda3/envs/Va/bin/python"
+
 
 ### The function takes the following inputs:
-# 1. sequence_length, 2. mu_msp (mutation rate for the msprime sim), 3. r_msp (recombination rate), 4. n_samples (number of diplod individuals to be sampled)
+# 1. sequence_length, 2. mu_msp (mutation rate for the msprime sim), 3. r_msp (recombination rate), 4. n_samples (number of diplod individuals to be sampled), 5. python_path (The path where python is tored on the system)
+
 
 ### Produces the following outputs
 # 1. c (the genotype matrix with rows being haplotypes, and columns being loci), 2. pos (positions of mutations)
@@ -22,20 +25,21 @@ sim_msprime_ancecstry = function(sequence_length,
                                  Ne, 
                                  mu_msp, 
                                  r_msp, 
-                                 n_sample) { # "n_samples" is the number of diploid individuals to be simulated (!Not genomes!)
+                                 n_sample, # "n_samples" is the number of diploid individuals to be simulated (!Not genomes!)
+                                 python_path) { # The path where python is stored
   
   
   # Specify the path to python
-  reticulate::use_python("/home/manas_ga/miniconda3/envs/Va/bin/python") #### !!!!! Needs to be edited !!!!!
+  reticulate::use_python(python_path)
   
   # Import msprim
   msprime <- reticulate::import("msprime")
   
   # Simulate the ancestry
-  ts <- msprime$sim_ancestry(n_sample, sequence_length=sequence_length, recombination_rate=r_msp, random_seed=42)
+  ts <- msprime$sim_ancestry(n_sample, sequence_length=sequence_length, recombination_rate=r_msp)
   
   # Simulate mutations
-  mts = msprime$sim_mutations(ts, rate=mu_msp, random_seed=5678)
+  mts = msprime$sim_mutations(ts, rate=mu_msp)
   
   print(paste("There are ", mts$num_sites, " sites with ", mts$num_mutations, " mutations"))
   
