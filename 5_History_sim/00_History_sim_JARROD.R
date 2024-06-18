374a1273e299b5782d263830550e5121533ea170
manas-ga <manas.geetaarun@ed.ac.uk>
2024-06-18 10:51:41 +0100

    Update 00_History_sim_JARROD.R

--- 5_History_sim/00_History_sim_JARROD.R
+++ 5_History_sim/00_History_sim_JARROD.R
@@ -23,7 +23,7 @@ rm(list = ls())
 
 ### Base path and path to Vw.Rmd (file containing Jarrod's functions) (depending on the system) ###
 
-if(Sys.info()["nodename"]=="bigshot"){
+if(Sys.info()["nodename"]=="bigfoot"){
   
   base_path = "/ceph/users/marun/Va_simulations/5_History_sim"
   Vw_path = "/ceph/users/marun/Va_simulations/6_Code_Test/Vw.Rmd"
