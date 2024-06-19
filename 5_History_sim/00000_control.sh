d2e6e1db6537f0c168653764660bca8107e72807
manas-ga <manas.geetaarun@ed.ac.uk>
2024-06-19 13:55:44 +0100

    Update 00000_control.sh

--- 5_History_sim/00000_control.sh
+++ 5_History_sim/00000_control.sh
@@ -5,7 +5,6 @@
 #$ -N Vw_test
 #$ -t 1-18
 #$ -tc 18
-#$ -l h_vmem=500G
 #$ -pe smp 1
 #$ -e /mnt/c/Users/msamant/Documents/GitHub/Va_simulations/5_History_sim/b_Interim_files/std_out/
 #$ -o /mnt/c/Users/msamant/Documents/GitHub/Va_simulations/5_History_sim/b_Interim_files/std_out/
