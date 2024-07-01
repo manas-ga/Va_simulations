8a19e1bdb9e99aa2597d4d74f5843e3f1da7c706
manas-ga <manas.geetaarun@ed.ac.uk>
2024-07-02 00:56:43 +0100

    Update 00000_control.sh

--- 5_History_sim/00000_control.sh
+++ 5_History_sim/00000_control.sh
@@ -6,7 +6,7 @@
 #$ -t 1-900
 #$ -tc 10
 #$ -l mem_free=130g,s_vmem=120g,h_vmem=150g
-#$ -pe smp64 15
+#$ -pe smp64 20
 #$ -e /ceph/users/marun/Va_simulations/5_History_sim/b_Interim_files/std_out/
 #$ -o /ceph/users/marun/Va_simulations/5_History_sim/b_Interim_files/std_out/
 
