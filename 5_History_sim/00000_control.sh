a2a34a31fb2b2bbad20db94b1f6f17a0a8b5616e
manas-ga <manas.geetaarun@ed.ac.uk>
2024-07-02 00:30:18 +0100

    Update 00000_control.sh
    Adjusted requirements for no-burnin sims

--- 5_History_sim/00000_control.sh
+++ 5_History_sim/00000_control.sh
@@ -3,10 +3,10 @@
 #$ -V
 #$ -cwd
 #$ -N Vw_no_burnin
-#$ -t 1-50
+#$ -t 1-900
 #$ -tc 10
-#$ -l mem_free=130g,s_vmem=1200g,h_vmem=150g
-#$ -pe smp64 20
+#$ -l mem_free=130g,s_vmem=120g,h_vmem=150g
+#$ -pe smp64 15
 #$ -e /ceph/users/marun/Va_simulations/5_History_sim/b_Interim_files/std_out/
 #$ -o /ceph/users/marun/Va_simulations/5_History_sim/b_Interim_files/std_out/
 
