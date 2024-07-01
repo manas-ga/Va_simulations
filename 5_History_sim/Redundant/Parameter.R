e3e66a0015f8cba69807e23b5fa32662dbf8ac21
manas-ga <manas.geetaarun@ed.ac.uk>
2024-07-01 13:07:18 +0100

    Parameter change

--- 5_History_sim/Redundant/Parameter.R
+++ 5_History_sim/Redundant/Parameter.R
@@ -3,7 +3,7 @@
 ########################################################################
 
 nsims = 100 # number of simulations for each set
-mu_list = seq(2.66e-09, 1.86e-08, length = nsims)
+mu_list = seq(2.25e-09, 2.25e-08, length = nsims)
 
 param_matrix = matrix(NA, nrow = 9, ncol  = 4)
 
