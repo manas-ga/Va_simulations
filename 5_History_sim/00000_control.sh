081fd57cd0521230591eb330ee25354ade602864
Manas Geeta Arun <manas.geetaarun@ed.ac.uk>
2024-07-02 14:49:58 +0100

    Update 00000_control.sh

--- 5_History_sim/00000_control.sh
+++ 5_History_sim/00000_control.sh
@@ -6,7 +6,7 @@
 #$ -t 1-90
 #$ -tc 10
 #$ -l mem_free=250g,s_vmem=350g,h_vmem=450g
-#$ -pe smp64 20
+#$ -pe smp64 22
 #$ -e /ceph/users/marun/Va_simulations/5_History_sim/b_Interim_files/std_out/
 #$ -o /ceph/users/marun/Va_simulations/5_History_sim/b_Interim_files/std_out/
 
