7a6d8f17c0b51dd291c3787e84fcaf280d2004d1
manas-ga <manas.geetaarun@ed.ac.uk>
2024-07-10 01:40:37 +0100

    Update 00_History_sim_JARROD.R

--- 5_History_sim/00_History_sim_JARROD.R
+++ 5_History_sim/00_History_sim_JARROD.R
@@ -269,7 +269,7 @@ for (sim in 1:nsims){
       
       mu = mu_list[sim]
       mu_msp = ifelse(end_gen==2, mu/532, mu/5320)
-      mu_neutral = ifelse(end_gen==2, mu_msp/3, mu/3)
+      mu_neutral = ifelse(end_gen==2, mu_msp/3, mu/180)
       
       message(paste("Simulation", sim, "in progress..."))
     
