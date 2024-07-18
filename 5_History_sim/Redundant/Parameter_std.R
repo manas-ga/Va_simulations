9aaea427b68f7ec480a676a51818f738afd642b4
Manas Geeta Arun <manas.geetaarun@ed.ac.uk>
2024-07-18 13:29:08 +0100

    Getting rid of quotes around parameters

--- 5_History_sim/Redundant/Parameter_std.R
+++ 5_History_sim/Redundant/Parameter_std.R
@@ -12,7 +12,7 @@
 nsims = 10 # number of simulations for each set
 mu_list = seq(5.56e-07, 5.56e-06, length = nsims)
 
-param_matrix = matrix(NA, nrow = nsims, ncol  = 7)
+param_matrix = matrix(NA, nrow = nsims, ncol  = 6)
 
 param_matrix[,1] = mu_list
 param_matrix[,2] = 1.4
@@ -20,7 +20,10 @@ param_matrix[,3] = 1.4
 param_matrix[,4] = 1000
 param_matrix[,5] = 10
 param_matrix[,6] = 3
-param_matrix[,7] = "fixed"
+bdelta_method = rep("fixed", nsims)
 
+param_matrix = data.frame(param_matrix)
 
-write.table(param_matrix, file = "/mnt/c/Users/msamant/Documents/GitHub/Va_simulations/5_History_sim/000_parameter_grid.txt", sep = " ", col.names = FALSE, row.names = FALSE)
+param_matrix[,7] = bdelta_method
+
+write.table(param_matrix, file = "/mnt/c/Users/msamant/Documents/GitHub/Va_simulations/5_History_sim/000_parameter_grid.txt", sep = " ", col.names = FALSE, row.names = FALSE, quote = FALSE)
