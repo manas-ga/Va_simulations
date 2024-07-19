003821d8a76fc44ba4acc09496157169fe83d352
manas-ga <manas.geetaarun@ed.ac.uk>
2024-07-19 15:59:23 +0100

    File structure for Eddie and AC3

--- 5_History_sim/00000_control.sh
+++ 5_History_sim/00000_control.sh
@@ -11,4 +11,6 @@
 #$ -o /ceph/users/marun/Va_simulations/5_History_sim/b_Interim_files/std_out/
 
 
-Rscript 00_History_sim_JARROD.R 1.2e-06 1.4 1.4 1000 10 3
+Param=`cat 000_parameter_grid.txt | awk "NR==$SGE_TASK_ID"`
+
+Rscript 00_History_sim_JARROD.R $Param
