2203c156d913b3e31f4b323e3ac5e9dadefe8966
manas-ga <manas.geetaarun@ed.ac.uk>
2024-07-19 16:11:50 +0100

    Create 00000_control_eddie.sh
    bash file for eddie with different -pe options

---
+++ 5_History_sim/00000_control_eddie.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#$ -V
+#$ -cwd
+#$ -N Vw_sim
+#$ -t 1-1
+#$ -tc 1
+#$ -l mem_free=250g,s_vmem=350g,h_vmem=450g
+#$ -pe smp 10
+#$ -j y
+#$ -o /ceph/users/marun/Va_simulations/5_History_sim/b_Interim_files/std_out/
+
+
+# std_out on Eddie: /exports/eddie/scratch/msamant/Va_simulations/b_Interim_files/std_out
+# std_out on AC3: /ceph/users/marun/Va_simulations/5_History_sim/b_Interim_files/std_out/
+
+
+Param=`cat 000_parameter_grid.txt | awk "NR==$SGE_TASK_ID"`
+
+Rscript 00_History_sim_JARROD.R $Param
