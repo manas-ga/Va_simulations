aa3880081a9b0d339c48043d1d1b9ef132c35502
manas-ga <manas.geetaarun@ed.ac.uk>
2024-07-19 19:08:52 +0100

    Update 00000_control.sh
    std_out/ path

--- 5_History_sim/00000_control.sh
+++ 5_History_sim/00000_control.sh
@@ -8,11 +8,11 @@
 #$ -l mem_free=250g,s_vmem=350g,h_vmem=450g,h=!bigyin
 #$ -pe smp64 10
 #$ -j y
-#$ -o /ceph/users/marun/Va_simulations/5_History_sim/b_Interim_files/std_out/
+#$ -o /data/obbard/Va_simulations/analyses/b_Interim_files/std_out/
 
 
 # std_out on Eddie: /exports/eddie/scratch/msamant/Va_simulations/b_Interim_files/std_out/
-# std_out on AC3: /ceph/users/marun/Va_simulations/5_History_sim/b_Interim_files/std_out/
+# std_out on AC3: /data/obbard/Va_simulations/analyses/b_Interim_files/std_out/
 
 
 Param=`cat 000_parameter_grid.txt | awk "NR==$SGE_TASK_ID"`
