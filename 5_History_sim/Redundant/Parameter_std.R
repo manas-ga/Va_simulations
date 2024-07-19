0419975fe41aa38b1a5b4989b1299335f77d06e4
manas-ga <manas.geetaarun@ed.ac.uk>
2024-07-19 11:50:41 +0100

    Reprinting parameters

--- 5_History_sim/Redundant/Parameter_std.R
+++ 5_History_sim/Redundant/Parameter_std.R
@@ -9,8 +9,8 @@
 # Col 5 = n_cages
 # Col 6 = ngen_expt
 
-nsims = 10 # number of simulations for each set
-mu_list = seq(3.45e-07, 3.45e-06, length = nsims)
+nsims = 1 # number of simulations for each set
+mu_list = seq(5.56e-07, 5.56e-06, length = nsims)
 
 param_matrix = matrix(NA, nrow = nsims, ncol  = 6)
 
