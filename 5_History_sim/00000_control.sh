290052dcc293468ed039f797802eaa6fe68fa00c
manas-ga <manas.geetaarun@ed.ac.uk>
2024-07-19 16:02:05 +0100

    Update 00000_control.sh

--- 5_History_sim/00000_control.sh
+++ 5_History_sim/00000_control.sh
@@ -11,6 +11,10 @@
 #$ -o /ceph/users/marun/Va_simulations/5_History_sim/b_Interim_files/std_out/
 
 
+# std_out on Eddie: /exports/eddie/scratch/msamant/Va_simulations/b_Interim_files/std_out
+# std_out on AC3: /ceph/users/marun/Va_simulations/5_History_sim/b_Interim_files/std_out/
+
+
 Param=`cat 000_parameter_grid.txt | awk "NR==$SGE_TASK_ID"`
 
 Rscript 00_History_sim_JARROD.R $Param
