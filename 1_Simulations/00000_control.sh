8097b51b53023b8127e1e9410e5be4a5433818e0
Manas Geeta Arun <manas.geetaarun@ed.ac.uk>
2024-12-11 18:24:06 +0000

    params

--- 1_Simulations/00000_control.sh
+++ 1_Simulations/00000_control.sh
@@ -6,7 +6,7 @@
 #$ -t 1-10
 #$ -tc 10
 #$ -l mem_free=250g,s_vmem=350g,h_vmem=450g
-#$ -pe smp64 2
+#$ -pe smp 3
 #$ -j y
 #$ -o /data/obbard/Va_simulations/analyses/b_Interim_files/std_out/
 
--- 2_Analysis/Analysis_script.R
+++ 2_Analysis/Analysis_script.R
@@ -164,5 +164,8 @@ for(sim in 1:nsims){
                               Ne = c(1000, 1000),
                               predict_Ne =  TRUE,                         # If true, this overwrites the Ne supplied above by Ne = c(nind_expt, predict_Ne(nind_expt, Ve_w_expt))
                               verbose = TRUE)
+  
+  message("Final output:")
+  print(analysed_data)
 
 }
