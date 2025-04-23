0d98d147489e64f356b525fb606a54a191f2e158
manas-ga <manas.geetaarun@ed.ac.uk>
2025-04-23 11:46:01 +0100

    params

--- 2_Analysis/00000_control_lost_va_new_ac3.sh
+++ 2_Analysis/00000_control_lost_va_new_ac3.sh
@@ -3,9 +3,9 @@
 #SBATCH --job-name=BC_V_A
 #SBATCH --output=/mnt/hel/obbard/Va_simulations/analyses/b_Interim_files/std_out/job_%A_%a.log   # Store logs in a custom directory
 #SBATCH --open-mode=append                                                                       # Append output if the file already exists
-#SBATCH --array=151-200%8                                                                       # Run replicate tasks
+#SBATCH --array=1-36%8                                                                       # Run replicate tasks
 #SBATCH --ntasks=10
-#SBATCH --mem=65G
+#SBATCH --mem=120G
 
 # Path to Conda (if Conda isn't initialized by default)
 CONDA_PATH="/home/msamant/miniconda3"  # Update to where Miniconda/Anaconda is installed
