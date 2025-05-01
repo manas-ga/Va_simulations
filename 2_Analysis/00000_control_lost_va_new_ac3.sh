eabb1881767f96e841adbaaa303135bb383e487e
manas-ga <manas.geetaarun@ed.ac.uk>
2025-05-01 13:11:51 +0100

    params for lost_va_script

--- 2_Analysis/00000_control_lost_va_new_ac3.sh
+++ 2_Analysis/00000_control_lost_va_new_ac3.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --job-name=BC_V_A
+#SBATCH --job-name=lost_vA
 #SBATCH --output=/mnt/hel/obbard/Va_simulations/analyses/b_Interim_files/std_out/job_%A_%a.log   # Store logs in a custom directory
 #SBATCH --open-mode=append                                                                       # Append output if the file already exists
-#SBATCH --array=1-200%8                                                                       # Run replicate tasks
+#SBATCH --array=1-80%10                                                                       # Run replicate tasks
 #SBATCH --ntasks=10
-#SBATCH --mem=180G
+#SBATCH --mem=30G
 
 # Path to Conda (if Conda isn't initialized by default)
 CONDA_PATH="/home/msamant/miniconda3"  # Update to where Miniconda/Anaconda is installed
@@ -16,4 +16,4 @@ conda activate marun
 
 Param=`cat lost_va_IDs.txt | awk "NR==${SLURM_ARRAY_TASK_ID}"`
 
-Rscript bc_va_script.R $Param
+Rscript lost_va_script.R $Param
