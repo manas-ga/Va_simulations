920fb9caf51e8088a69ac4f5f8df81d0866e0ec1
manas-ga <manas.geetaarun@ed.ac.uk>
2025-05-02 14:38:07 +0100

    params

--- 1_Simulations/00000_control_new_ac3.sh
+++ 1_Simulations/00000_control_new_ac3.sh
@@ -3,9 +3,9 @@
 #SBATCH --job-name=Set_16
 #SBATCH --output=/mnt/hel/obbard/Va_simulations/analyses/b_Interim_files/std_out/job_%A_%a.log   # Store logs in a custom directory
 #SBATCH --open-mode=append                                                                       # Append output if the file already exists
-#SBATCH --array=1-100%25                                                                          # Run replicate tasks
+#SBATCH --array=1-73%8                                                                          # Run replicate tasks
 #SBATCH --ntasks=2
-#SBATCH --mem=20G
+#SBATCH --mem=200G
 
 # Path to Conda (if Conda isn't initialized by default)
 CONDA_PATH="/home/msamant/miniconda3"  # Update to where Miniconda/Anaconda is installed
--- 1_Simulations/Misc_scripts_files/Create_parameter_grid_std.R
+++ 1_Simulations/Misc_scripts_files/Create_parameter_grid_std.R
@@ -19,10 +19,10 @@ standard_only = FALSE
 ## The standard set (to be used as a reference to compare)
 
 nsims = 25 # number of simulations for each set
-end_gen = 2
+end_gen = 25000
 
-mu_msp_list = if(end_gen==2){seq(3e-9, 2.35e-8, length = nsims)}else{seq(3.6e-8, 3.6e-7, length = nsims)}
-mut_ratio = if(end_gen==2){1}else{0.00}
+mu_msp_list = if(end_gen==2){seq(3e-9, 2.35e-8, length = nsims)}else{seq(5.56e-8, 5.56e-7, length = nsims)}
+mut_ratio = if(end_gen==2){1}else{0}
 
 
 param_matrix = data.frame(matrix(NA, nrow = nsims, ncol  = 8))
