5738bd2908cf5a3d3f9cd3f147b0e146dceb6fdc
manas-ga <manas.geetaarun@ed.ac.uk>
2025-05-05 11:00:51 +0100

    Update 00000_control_eddie.sh

--- 1_Simulations/00000_control_eddie.sh
+++ 1_Simulations/00000_control_eddie.sh
@@ -8,10 +8,10 @@
 #$ -l h_vmem=4g
 #$ -pe sharedmem 2
 #$ -j y
-#$ -o /exports/eddie/scratch/msamant/Va_simulations/b_Interim_files/std_out/
+#$ -o /exports/eddie/scratch/msamant/b_Interim_files/std_out/
 
 
-# std_out on Eddie: /exports/eddie/scratch/msamant/Va_simulations/b_Interim_files/std_out/
+# std_out on Eddie: /exports/eddie/scratch/msamant/b_Interim_files/std_out/
 # std_out on AC3: /ceph/users/marun/Va_simulations/5_History_sim/b_Interim_files/std_out/
 
 
