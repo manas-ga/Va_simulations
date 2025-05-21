c9091cf2c555326c319527d69341a63319118237
manas-ga <manas.geetaarun@ed.ac.uk>
2025-05-21 18:59:54 +0100

    Set_18 params

--- 2_Analysis/00000_control_analysis_new_ac3.sh
+++ 2_Analysis/00000_control_analysis_new_ac3.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --job-name=Set_17_re
+#SBATCH --job-name=Set_18_re
 #SBATCH --output=/mnt/hel/obbard/Va_simulations/analyses/b_Interim_files/std_out/job_%A_%a.log   # Store logs in a custom directory
 #SBATCH --open-mode=append                                                                       # Append output if the file already exists
-#SBATCH --array=1-69%10                                                                          # Run replicate tasks
-#SBATCH --ntasks=12
-#SBATCH --mem=300G
+#SBATCH --array=1-900%9                                                                          # Run replicate tasks
+#SBATCH --ntasks=5
+#SBATCH --mem=10G
 
 # Path to Conda (if Conda isn't initialized by default)
 CONDA_PATH="/home/msamant/miniconda3"  # Update to where Miniconda/Anaconda is installed
@@ -14,6 +14,6 @@ source $CONDA_PATH/etc/profile.d/conda.sh  # Initialize Conda in the job script
 # Activate the Conda environment
 conda activate marun
 
-Param=`cat analysis_param_grid.txt | awk "NR==${SLURM_ARRAY_TASK_ID}"`
+Param=`cat analysis_param_grid_ac3.txt | awk "NR==${SLURM_ARRAY_TASK_ID}"`
 
 Rscript Analysis_script.R $Param
