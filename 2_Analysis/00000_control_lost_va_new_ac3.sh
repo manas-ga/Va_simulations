934221ff98534617abfab9dab98e1b9bbe292b65
manas-ga <manas.geetaarun@ed.ac.uk>
2025-05-29 15:38:09 +0100

    params lost_va_script for Set_N1 (4 sims)

--- 2_Analysis/00000_control_lost_va_new_ac3.sh
+++ 2_Analysis/00000_control_lost_va_new_ac3.sh
@@ -3,9 +3,9 @@
 #SBATCH --job-name=lost_vA_Set_18
 #SBATCH --output=/mnt/hel/obbard/Va_simulations/analyses/b_Interim_files/std_out/job_%A_%a.log   # Store logs in a custom directory
 #SBATCH --open-mode=append                                                                       # Append output if the file already exists
-#SBATCH --array=1-900%15                                                                       # Run replicate tasks
-#SBATCH --ntasks=5
-#SBATCH --mem=10G
+#SBATCH --array=1-4%4                                                                       # Run replicate tasks
+#SBATCH --ntasks=12
+#SBATCH --mem=120G
 
 # Path to Conda (if Conda isn't initialized by default)
 CONDA_PATH="/home/msamant/miniconda3"  # Update to where Miniconda/Anaconda is installed
