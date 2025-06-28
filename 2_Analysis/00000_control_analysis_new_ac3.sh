d7a06821812adb327b48e5fcd77732854b0ab516
Manas Geeta Arun <manas.geetaarun@ed.ac.uk>
2025-06-28 21:48:07 +0100

    params

--- 2_Analysis/00000_control_analysis_new_ac3.sh
+++ 2_Analysis/00000_control_analysis_new_ac3.sh
@@ -3,9 +3,9 @@
 #SBATCH --job-name=pool_seq_sims
 #SBATCH --output=/mnt/hel/obbard/Va_simulations/analyses/b_Interim_files/std_out/job_%A_%a.log   # Store logs in a custom directory
 #SBATCH --open-mode=append                                                                       # Append output if the file already exists
-#SBATCH --array=21-30%10                                                                          # Run replicate tasks
-#SBATCH --ntasks=5
-#SBATCH --mem=10G
+#SBATCH --array=1-100%10                                                                          # Run replicate tasks
+#SBATCH --ntasks=12
+#SBATCH --mem=250G
 
 # Path to Conda (if Conda isn't initialized by default)
 CONDA_PATH="/home/msamant/miniconda3"  # Update to where Miniconda/Anaconda is installed
