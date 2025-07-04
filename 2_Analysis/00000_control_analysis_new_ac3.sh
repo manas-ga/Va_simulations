18197b36d4b058b840508f22e15c2590ce1c2567
Manas Geeta Arun <manas.geetaarun@ed.ac.uk>
2025-07-04 16:14:22 +0100

    Test_dom params

--- 2_Analysis/00000_control_analysis_new_ac3.sh
+++ 2_Analysis/00000_control_analysis_new_ac3.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --job-name=pool_seq_sims
+#SBATCH --job-name=Test_dom
 #SBATCH --output=/mnt/hel/obbard/Va_simulations/analyses/b_Interim_files/std_out/job_%A_%a.log   # Store logs in a custom directory
 #SBATCH --open-mode=append                                                                       # Append output if the file already exists
-#SBATCH --array=1-100%10                                                                          # Run replicate tasks
-#SBATCH --ntasks=12
-#SBATCH --mem=250G
+#SBATCH --array=1-40%20                                                                          # Run replicate tasks
+#SBATCH --ntasks=2
+#SBATCH --mem=5G
 
 # Path to Conda (if Conda isn't initialized by default)
 CONDA_PATH="/home/msamant/miniconda3"  # Update to where Miniconda/Anaconda is installed
--- 2_Analysis/00000_control_lost_va_new_ac3.sh
+++ 2_Analysis/00000_control_lost_va_new_ac3.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --job-name=lost_vA_Set_18
+#SBATCH --job-name=Test_dom
 #SBATCH --output=/mnt/hel/obbard/Va_simulations/analyses/b_Interim_files/std_out/job_%A_%a.log   # Store logs in a custom directory
 #SBATCH --open-mode=append                                                                       # Append output if the file already exists
-#SBATCH --array=1-4%4                                                                       # Run replicate tasks
-#SBATCH --ntasks=12
-#SBATCH --mem=120G
+#SBATCH --array=1-40%20                                                                       # Run replicate tasks
+#SBATCH --ntasks=2
+#SBATCH --mem=5G
 
 # Path to Conda (if Conda isn't initialized by default)
 CONDA_PATH="/home/msamant/miniconda3"  # Update to where Miniconda/Anaconda is installed
@@ -14,6 +14,6 @@ source $CONDA_PATH/etc/profile.d/conda.sh  # Initialize Conda in the job script
 # Activate the Conda environment
 conda activate marun
 
-Param=`cat lost_va_IDs.txt | awk "NR==${SLURM_ARRAY_TASK_ID}"`
+Param=`cat  analysis_param_grid_ac3.txt | awk "NR==${SLURM_ARRAY_TASK_ID}"`
 
 Rscript lost_va_script.R $Param
