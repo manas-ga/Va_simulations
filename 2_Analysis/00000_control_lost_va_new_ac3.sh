164c9c89e2011d3a4ef91fac2ea8a5211361ccb9
Manas Geeta Arun <manas.geetaarun@ed.ac.uk>
2025-07-05 12:48:31 +0100

    params

--- 2_Analysis/00000_control_lost_va_new_ac3.sh
+++ 2_Analysis/00000_control_lost_va_new_ac3.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#SBATCH --job-name=Test_dom
+#SBATCH --job-name=Set_21
 #SBATCH --output=/mnt/hel/obbard/Va_simulations/analyses/b_Interim_files/std_out/job_%A_%a.log   # Store logs in a custom directory
 #SBATCH --open-mode=append                                                                       # Append output if the file already exists
-#SBATCH --array=1-40%20                                                                       # Run replicate tasks
+#SBATCH --array=1-20%25                                                                       # Run replicate tasks
 #SBATCH --ntasks=2
 #SBATCH --mem=5G
 
