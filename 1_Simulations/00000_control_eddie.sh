cf5d8cb964b57cdf4a81e84a61ce0d0e3a31bb60
Manas Geeta Arun <manas.geetaarun@ed.ac.uk>
2025-07-22 11:41:26 +0100

    Set_25_C

--- 1_Simulations/00000_control_eddie.sh
+++ 1_Simulations/00000_control_eddie.sh
@@ -2,10 +2,10 @@
 
 #$ -V
 #$ -cwd
-#$ -N Set_25_A
+#$ -N Set_25_C
 #$ -t 1-100
 #$ -tc 100
-#$ -l h_rt=24:10:00
+#$ -l h_rt=12:10:00
 #$ -l h_vmem=10g
 #$ -pe sharedmem 2
 #$ -j y
--- 1_Simulations/Misc_scripts_files/Create_parameter_grid_std.R
+++ 1_Simulations/Misc_scripts_files/Create_parameter_grid_std.R
@@ -18,10 +18,10 @@ standard_only = TRUE
 
 ## The standard set (to be used as a reference to compare)
 
-nsims = 10 # number of simulations for each set
+nsims = 100 # number of simulations for each set
 end_gen = 25000
 
-mu_msp_list = if(end_gen==2){seq(3e-9, 2.35e-8, length = nsims)}else{seq(7e-08, 2e-07, length = nsims)}
+mu_msp_list = if(end_gen==2){seq(3e-9, 2.35e-8, length = nsims)}else{seq(3.6e-08, 3.6e-07, length = nsims)}
 mut_ratio = if(end_gen==2){1}else{0}
 
 
@@ -100,4 +100,4 @@ if(!standard_only){
   
 }
 
-write.table(noquote(param_matrix), file = "/mnt/c/Users/msamant/Documents/GitHub/Va_simulations/1_Simulations/000_parameter_grid_ac3.txt", sep = " ", col.names = FALSE, row.names = FALSE, quote = FALSE)
+write.table(noquote(param_matrix), file = "/mnt/c/Users/msamant/Documents/GitHub/Va_simulations/1_Simulations/000_parameter_grid_eddie.txt", sep = " ", col.names = FALSE, row.names = FALSE, quote = FALSE)
