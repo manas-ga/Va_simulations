800883f3cc54cbc1cf9d80864fc993f16f1a5d72
manas-ga <manas.geetaarun@ed.ac.uk>
2025-08-04 09:37:30 +0100

    Set_31 ac3 params

--- 1_Simulations/00000_control_new_ac3.sh
+++ 1_Simulations/00000_control_new_ac3.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --job-name=TEST
+#SBATCH --job-name=Set_31
 #SBATCH --output=/mnt/hel/obbard/Va_simulations/analyses/b_Interim_files/std_out/job_%A_%a.log   # Store logs in a custom directory
 #SBATCH --open-mode=append                                                                       # Append output if the file already exists
-#SBATCH --array=1-10%10                                                                         # Run replicate tasks
+#SBATCH --array=1-300%30                                                                         # Run replicate tasks
 #SBATCH --ntasks=2
-#SBATCH --mem=5G
+#SBATCH --mem=10G
 
 # Path to Conda (if Conda isn't initialized by default)
 CONDA_PATH="/home/msamant/miniconda3"  # Update to where Miniconda/Anaconda is installed
