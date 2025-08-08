27143da80a3e9fbba1fc0a3ec34b77c27eae493c
manas-ga <manas.geetaarun@ed.ac.uk>
2025-08-08 14:00:03 +0100

    Set_28 params

--- 1_Simulations/00000_control_new_ac3.sh
+++ 1_Simulations/00000_control_new_ac3.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#SBATCH --job-name=Test_dom
+#SBATCH --job-name=Set_28
 #SBATCH --output=/mnt/hel/obbard/Va_simulations/analyses/b_Interim_files/std_out/job_%A_%a.log   # Store logs in a custom directory
 #SBATCH --open-mode=append                                                                       # Append output if the file already exists
-#SBATCH --array=1-40%40                                                                         # Run replicate tasks
+#SBATCH --array=1-100%25                                                                         # Run replicate tasks
 #SBATCH --ntasks=2
 #SBATCH --mem=10G
 
