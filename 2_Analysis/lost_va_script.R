d0c40fa243616f2410c528d39f0e132e4cc0e0f2
manas-ga <manas.geetaarun@ed.ac.uk>
2025-08-19 18:57:06 +0100

    params for Set_32 new_va task 279

--- 2_Analysis/lost_va_script.R
+++ 2_Analysis/lost_va_script.R
@@ -147,7 +147,7 @@ if(Sys.info()["nodename"]!="SCE-BIO-C06645"|Sys.info()["nodename"]!="sce-bio-c04
 #install.packages(Vw_library_path, repos = NULL, type = "source")
 library(Vw)
 
-finescale = TRUE # Whether a file containing data on locus-wise va_lost vs alpha should be saved
+finescale = FALSE # Whether a file containing data on locus-wise va_lost vs alpha should be saved
 verbose = TRUE
 
 ########################
